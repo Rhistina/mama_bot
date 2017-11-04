159037c80c3e123c19a01c588f65d637c94d8b8c
Orenshi <rhistina@gmail.com>
2017-11-03 21:20:14 -0400

    Add functions that return iterable dict results One to fetch all records and turn to list of dicts, another to fetch for N number of records and return generator of dicts i.e. in case there's a lot of records to page through and don't want read all of them in memory)

--- mama_bot/utils/db.py
+++ mama_bot/utils/db.py
@@ -1,4 +1,5 @@
 import psycopg2 as pg
+from psycopg2.extras import DictCursor
 import os
 
 class DB:
@@ -13,7 +14,7 @@ class DB:
         self._connect()
 
     def __enter__(self):
-        self.connection.autocommit = False
+        self.conn.autocommit = False
         return self
 
     def __exit__(self, *exc):
@@ -35,24 +36,66 @@ class DB:
                                                                    self.password)
 
     def _connect(self):
-        self.connection = pg.connect(**self._connection_args())
-        self.connection.autocommit = self.autocommit
+        self.conn = pg.connect(**self._connection_args())
+        self.conn.autocommit = self.autocommit
         print("Connected to {}".format(self.host))
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("select version()")
-        print(self.cursor.fetchall())
+        self.cur = self.conn.cursor(cursor_factory=DictCursor)
+        self.cur.execute("select version()")
+        print(self.cur.fetchall())
 
     def _close(self):
-        self.connection.commit()
-        self.connection.close()
+        self.conn.commit()
+        self.conn.close()
 
-    def execute(self, sql):
+    def execute(self, sql : str ):
+        """
+        Execute an query that returns no results
+        I.E. an insert, update, delete etc.
+        :param sql:
+        :return:
+        """
         try:
-            self.cursor.execute(sql)
-            # return self.cursor.fetchall()
+            self.cur.execute(sql)
         except Exception as e:
             print("Unable to execute query")
             # in a context manager any failures here would automatically rollback() but doing it for good measure
-            self.connection.rollback()
+            self.conn.rollback()
             raise e
+
+    def query_to_list(self, sql):
+        """
+        Get results of a query in list form
+        :param sql:
+        :return: a list of dictionaries
+        """
+        try:
+            self.cur.execute(sql)
+            if self.cur.rowcount <= 0:
+                return []
+
+            return [dict(row) for row in self.cur]
+
+        except Exception as e:
+            print("Unable to execute query")
+            # in a context manager any failures here would automatically rollback() but doing it for good measure
+            self.conn.rollback()
+            raise e
+
+    def query_to_iter(self, sql, size=1000):
+        """
+        Get the results of a query in a generator
+        :param sql:
+        :param size:
+        :return: a generator of dictionaries
+        """
+        self.cur.execute(sql)
+        while True:
+            try:
+                results = self.cur.fetchmany(size)
+                if not results: break
+                for row in results:
+                    yield dict(row)
+            except Exception as e:
+                print("Unable to execute query")
+                raise e
 
