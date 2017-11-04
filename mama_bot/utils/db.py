38693d457ed48bfec8acc37fb98d3804b50fd852
Orenshi <rhistina@gmail.com>
2017-11-03 20:35:13 -0400

    Can either instantiate DB in autocommit=True mode. Or use DB in context manager for transaction commits

--- mama_bot/utils/db.py
+++ mama_bot/utils/db.py
@@ -1,9 +1,7 @@
 import psycopg2 as pg
 import os
-from contextlib import contextmanager
 
 class DB:
-
     def __init__(self, **kwargs):
 
         self.host = kwargs.get('host') if 'host' in kwargs else os.getenv("DB_HOST")
@@ -11,13 +9,15 @@ class DB:
         self.dbname = kwargs.get('dbname') if 'dbname' in kwargs else os.getenv("DB_NAME")
         self.user = kwargs.get('user') if 'user' in kwargs else os.getenv("DB_USER")
         self.password = kwargs.get('password') if 'user' in kwargs else os.getenv("DB_PASSWORD")
+        self.autocommit = kwargs.get('autocommit', True)
+        self._connect()
 
     def __enter__(self):
-        ...
+        self.connection.autocommit = False
+        return self
 
-    def __exit__(self):
-        ...
-
+    def __exit__(self, *exc):
+        self._close()
 
     def _connection_args(self) -> dict:
         return  {'host':self.host,
@@ -34,18 +34,25 @@ class DB:
                                                                    self.user,
                                                                    self.password)
 
-    @contextmanager
-    def connect(self):
+    def _connect(self):
+        self.connection = pg.connect(**self._connection_args())
+        self.connection.autocommit = self.autocommit
+        print("Connected to {}".format(self.host))
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("select version()")
+        print(self.cursor.fetchall())
+
+    def _close(self):
+        self.connection.commit()
+        self.connection.close()
+
+    def execute(self, sql):
         try:
-            with pg.connect(**self._connection_args()) as conn:
-                with conn.cursor() as cursor:
-                    yield cursor
-            conn.close()
+            self.cursor.execute(sql)
+            # return self.cursor.fetchall()
         except Exception as e:
-            print('Unable to run query')
+            print("Unable to execute query")
+            # in a context manager any failures here would automatically rollback() but doing it for good measure
+            self.connection.rollback()
             raise e
 
-# db = DB()
-# with db.connect() as cur:
-#     cur.execute("select version()")
-#     print(cur.fetchall())
